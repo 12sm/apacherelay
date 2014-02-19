2cc3bee804f7d49c86e02864ed7f649dc3e79992
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-02-19 14:05:01 -0600

    formatting

--- base.php
+++ base.php
@@ -19,7 +19,7 @@
 
   <div class="wrap container" role="document">
     <div class="content row">
-      <main class="main col-sm-10 col-sm-offset-1" role="main">
+      <main class="main col-sm-8 col-sm-offset-2" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
     </div><!-- /.content -->
