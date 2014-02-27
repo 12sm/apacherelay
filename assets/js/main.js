3e6d0e065db33247f0a70f884b8b79d9ac3eef4a
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-02-27 15:43:38 -0600

    js attempt

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,7 @@ $(window).load(function() {
     } else {
       element.attachEvent(eventName, callback, false);
     }
-  }
+  };
  
   function ready(player_id) {
     var froogaloop = $f(player_id);
@@ -78,7 +78,7 @@ $(window).load(function() {
     froogaloop.addEvent('pause', function(data) {
       $('.flexslider').flexslider("play");
     });
-  }
+  };
  
    
   // Call fitVid before FlexSlider initializes, so the proper initial height can be retrieved.
