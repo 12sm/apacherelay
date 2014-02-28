dba3459e78b1c5f0a9c78482943238bb063d3366
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-02-28 10:58:49 -0600

    moved bgVid to header-top-navbar

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -21,5 +21,9 @@
       </div>
       </div>
     </nav>
+    <video autoplay loop poster="/assets/img/back.jpg" id="bgvid">
+    <source src="/wp-content/themes/apacherelay/assets/img/back.webm" type="video/webm">
+      <source src="/wp-content/themes/apacherelay/assets/img/back.mp4" type="video/mp4">
+    </video>
   </div>
 </header>
