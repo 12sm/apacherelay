45d4e77ed1453152021df23c84945b37c813ebe6
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-12 15:19:01 -0500

    where is the content?

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
+  <!--before content-->
   <?php the_content(); ?>
+  <!--after the content-->
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
