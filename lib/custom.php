28b33f138e194b328bc973ee4e8f2c0bf865297c
michael <michael@12southmusic.com>
2014-03-12 17:28:38 -0500

    return result

--- lib/custom.php
+++ lib/custom.php
@@ -1,13 +1,10 @@
 <?php
 function youtube_id_from_url($atts) { 
-	/* extract(shortcode_atts(array(
+	extract(shortcode_atts(array(
     'url' => '00000',
    ), $atts));   
     $result = preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
-    if (false !== $result) {
-        return $matches[1];
-    } */
-    return Awesome;
+    return $result;
 }
 
 
