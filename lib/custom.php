a153260ef18e2cafb96c82e137ada70b1fcbdcf7
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-12 18:10:56 -0500

    iframe error

--- lib/custom.php
+++ lib/custom.php
@@ -6,8 +6,5 @@ function youtube_id_from_url($atts) {
     $result = preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $url, $matches);
     return $matches[0];
 }
-
-
 add_shortcode('youtube_id', 'youtube_id_from_url');
-
 ?>
