fbcbffaacf8cc892ae9100514daa8815c9eee550
michael <michael@12southmusic.com>
2014-03-13 14:36:15 -0500

    different xpos

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,7 @@ var Roots = {
 		//if the first section is in view...
 		if($firstBG.hasClass("inview")){
 			//call the newPos function and change the background position
-			$firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, -200, 0.5)}); 
+			$firstBG.css({'backgroundPosition': newPos(-290px, windowHeight, pos, -200, 0.5)}); 
 		}
 		
 		//if the second section is in view...
