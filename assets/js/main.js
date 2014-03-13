5ae0f1a26ba0a4f37ada4464fae90d9763d1d05e
michael <michael@12southmusic.com>
2014-03-13 16:02:09 -0500

    Background pos 2 and 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,13 +100,13 @@ var Roots = {
 		//if the second section is in view...
 		if($secondBG.hasClass("inview")){
 			//call the newPos function and change the background position
-			$secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, -100, -0.5)});
+			$secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, -0.5)});
 		}
 		
 		//if the third section is in view...
 		if($thirdBG.hasClass("inview")){
 			//call the newPos function and change the background position
-			$thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 600, -0.5)});
+			$thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 800, -0.5)});
 		}
 		
 		//if the fourth section is in view...
