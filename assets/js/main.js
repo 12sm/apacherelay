f7d95a5107d677eeea7b948197c1aee4eebabf45
michael <michael@12southmusic.com>
2014-03-13 14:29:13 -0500

    Change first bg speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ var Roots = {
 		//if the first section is in view...
 		if($firstBG.hasClass("inview")){
 			//call the newPos function and change the background position
-			$firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 100, 0.3)}); 
+			$firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 100, 0.5)}); 
 		}
 		
 		//if the second section is in view...
