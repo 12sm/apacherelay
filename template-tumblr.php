0b5bf86df883dc1eecc0faa6a9deb19816095e47
Ian Noyes <ian.noyes@pop.belmont.edu>
2014-03-18 17:06:19 -0500

    tumblr-template?

---
+++ template-tumblr.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Tumblr Template
+*/
+?>
+<?php
+  $request_url = 'http://http://apache-relay-official.tumblr.com/api/read?start=0&num=1';
+  $xml = simplexml_load_file($request_url);
+  $title = $xml->posts->post->{'regular-title'};
+  $link = $xml->posts->post[‘url’];
+  echo 'Latest blog entry: <a href="'.$link.'">'.$title.'</a>';
+?>
+<p class="blog_title"><?php include('includes/latest_blog.php'); ?></p>
