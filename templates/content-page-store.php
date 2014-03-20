4aabcf6562dbf63f3d55e893541bce1c3a51ba8d
michael <michael@12southmusic.com>
2014-03-20 17:03:25 -0500

    Totally update the Store Page dude

--- templates/content-page-store.php
+++ templates/content-page-store.php
@@ -1,11 +1,7 @@
 <div class="row store-page">
 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
-<?php while (have_posts()) : the_post(); ?>
-<?php the_content(); ?> 
-
-
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
-
+<div class="row">
+<?php echo do_shortcode('[wpv-view name="Store Page"]'); ?>
+</div>
 </div>
 </div>
