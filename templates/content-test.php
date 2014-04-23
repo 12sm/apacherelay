ac72205e9cc3cfb1b162b29651437d3fcccf22ee
Ian Noyes <ian@12southmusic.com>
2014-04-23 11:47:03 -0500

    making test templates

---
+++ templates/content-test.php
@@ -0,0 +1,22 @@
+<?php while (have_posts()) : the_post(); ?>
+<?php the_content(); ?> 
+  
+<?php
+
+echo do_shortcode('[wpv-view name="Home-video"]');
+
+echo do_shortcode('[wpv-view name="Music"]');
+
+echo do_shortcode('[wpv-view name="Tour"]');
+
+echo do_shortcode('[wpv-view name="blog-home"]');
+
+echo do_shortcode('[wpv-view name="Gallery-home"]');
+
+echo do_shortcode('[wpv-view name="Store"]');
+
+?>
+
+
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
