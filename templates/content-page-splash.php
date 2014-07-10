c4a8d41acc1bf7d6a0e4f9da5c3983ac8681a4df
Ian Noyes <ian@12southmusic.com>
2014-07-10 15:54:01 -0500

    do shortcode for bg vid

--- templates/content-page-splash.php
+++ templates/content-page-splash.php
@@ -1,5 +1,6 @@
 <div class="row">
 	<div class="col-sm-12">
+	<?php echo do_shortcode('[mbYTPlayer url="http://youtu.be/Kb5HdMQXMls" opacity="1" quality="default" ratio="auto" isinline="false" showcontrols="false" realfullscreen="true" printurl="true" autoplay="true" mute="true" loop="true" addraster="false"] '); ?>
 		<?php while (have_posts()) : the_post(); ?>
 	 	<?php the_content(); ?>
 		<?php endwhile; ?>
