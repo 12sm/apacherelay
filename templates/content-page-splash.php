32cdd763589908f6c76fd1513711eb71beeb0ebd
michael <michael@12southmusic.com>
2014-07-11 13:36:54 -0500

    Updated youtube vid

--- templates/content-page-splash.php
+++ templates/content-page-splash.php
@@ -1,4 +1,4 @@
-	<?php echo do_shortcode('[mbYTPlayer url="http://youtu.be/Kb5HdMQXMls" opacity="1" quality="default" ratio="auto" isinline="false" showcontrols="false" realfullscreen="true" printurl="true" autoplay="true" mute="true" loop="true" addraster="false"] '); ?>
+	<?php echo do_shortcode('[mbYTPlayer url="http://youtu.be/kRqk4M3hn-c" opacity="1" quality="default" ratio="auto" isinline="false" showcontrols="false" realfullscreen="true" printurl="true" autoplay="true" mute="true" loop="true" addraster="false"] '); ?>
 	<?php while (have_posts()) : the_post(); ?>
 	 	<?php the_content(); ?>
 		<?php endwhile; ?>
