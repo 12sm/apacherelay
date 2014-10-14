54821317fedcd4313d4df2ccce76312a904ee1f5
Ian Noyes <ian@12southmusic.com>
2014-10-14 15:27:51 -0500

    blamo

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,13 +255,13 @@ var Roots = {
     //if the first section is in view...
     if($firstBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $firstBG.css({'backgroundPosition': newPos(20, windowHeight, pos, 2100, -0.4)}); 
+      $firstBG.css({'backgroundPosition': newPos(20, windowHeight, pos, 3900, -0.4)}); 
     }
      
     //if the second section is in view...
     if($secondBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 1900, -0.4)});
+      $secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2300, -0.4)});
     }
     
     //if the third section is in view...
