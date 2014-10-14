73a710395097f3cd39eb70100df5a12288f6e63b
Ian Noyes <ian@12southmusic.com>
2014-10-14 13:20:40 -0500

    try it

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,31 +255,31 @@ var Roots = {
     //if the first section is in view...
     if($firstBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $firstBG.css({'backgroundPosition': newPos(20, windowHeight, pos, 0, -0.2)}); 
+      $firstBG.css({'backgroundPosition': newPos(20, windowHeight, pos, 700, -0.4)}); 
     }
     
     //if the second section is in view...
     if($secondBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, -0.2)});
+      $secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, -0.4)});
     }
     
     //if the third section is in view...
     if($thirdBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 700, -0.2)});
+      $thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 700, -0.4)});
     }
     
     //if the fourth section is in view...
     if($fourthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, -0.2)});   
+      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, -0.4)});   
     }
     
     //if the fifth section is in view...
     if($fifthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, -0.2)});
+      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 1100, -0.4)});
     }
     
   }
