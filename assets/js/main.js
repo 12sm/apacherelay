daf9d46f652b07362bab343a35b28d03fd81bfca
Ian Noyes <ian@12southmusic.com>
2014-10-14 15:39:15 -0500

    mo plax mo problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,13 +273,13 @@ var Roots = {
     //if the fourth section is in view...
     if($fourthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2900, -0.4)});   
+      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 3600, -0.4)});   
     }
     
     //if the fifth section is in view...
     if($fifthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 4700, -0.4)});
+      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 5100, -0.4)});
     }
     
   }
