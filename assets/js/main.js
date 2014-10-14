955ef35c601fd2f4a4596dc8cd0fc2d54ecf1f2e
Ian Noyes <ian@12southmusic.com>
2014-10-14 16:26:08 -0500

    for the big screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,13 +273,13 @@ var Roots = {
     //if the fourth section is in view...
     if($fourthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 4600, -0.4)});   
+      $fourthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 5000, -0.4)});   
     }
     
     //if the fifth section is in view...
     if($fifthBG.hasClass("inview")){
       //call the newPos function and change the background position
-      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 6100, -0.4)});
+      $fifthBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 6400, -0.4)});
     }
     
   }
