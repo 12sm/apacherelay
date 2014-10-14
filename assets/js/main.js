f1ca19083b983b63b88bb55db5ba0b1129b4d4ca
Ian Noyes <ian@12southmusic.com>
2014-10-13 23:10:32 -0500

    getting desperate

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,15 +16,7 @@
 
 (function($) {
 
-  function bgImg(){
-    $imgBoxCont = $(this)
-    $img = $('img:first',$imgBoxCont);
-      if ($imgBoxCont.css('background-image').indexOf(encodeURI($img.attr('src'))) === -1) {
-      // Change
-      $imgBoxCont.css({'background-image': 'url("' + encodeURI($img.attr('src')) + '")'});
-    }
-    if (!$img.length) {console.log('No image found');} 
-  }
+  
 
 // Use this variable to set up the common and page specific functions. If you 
 // rename this variable, you will also need to rename the namespace below.
@@ -209,6 +201,15 @@ var Roots = {
   },
     test: {
     init: function() {
+      function bgImg(){
+    $imgBoxCont = $(this)
+    $img = $('img:first',$imgBoxCont);
+      if ($imgBoxCont.css('background-image').indexOf(encodeURI($img.attr('src'))) === -1) {
+      // Change
+      $imgBoxCont.css({'background-image': 'url("' + encodeURI($img.attr('src')) + '")'});
+    }
+    if (!$img.length) {console.log('No image found');} 
+  }
       $('.bg-img').bgImg();
     }
   }
