6f1289ae09391b56fe248dd82f314810bd8f65a0
Ian Noyes <ian@12southmusic.com>
2014-10-15 14:28:39 -0500

    no landscape

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,7 +299,7 @@ var Roots = {
     
   }
   
-  enquire.register("not screen and (orientation : landscape) and (max-width : 992px)", function() {
+  enquire.register("not screen and (orientation : landscape)", function() {
         $window.resize(function(){ //if the user resizes the window...
       Move(); //move the background images in relation to the movement of the scrollbar
     });   
