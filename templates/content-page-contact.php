8f2851742b5da0219b5ba87511082f9a6e502a09
Ian Noyes <ian@12southmusic.com>
2014-10-15 12:52:16 -0500

    contact background

--- templates/content-page-contact.php
+++ templates/content-page-contact.php
@@ -1,3 +1,4 @@
+<?php echo do_shortcode('[wpv-view name="BACKGROUND IMAGE | PAGE" ids="1457"]');?>
 <div class="row">
 	<div class="col-sm-12 contact-page">
 <?php while (have_posts()) : the_post(); ?>
--- templates/content-page-music.php
+++ templates/content-page-music.php
@@ -1,2 +1,2 @@
-<?php echo do_shortcode('[wpv-view name="BACKGROUND IMAGE | PAGE" ids="1441"]');?>
+
 <?php echo do_shortcode('[wpv-view name="Spotify Player"]'); ?>
