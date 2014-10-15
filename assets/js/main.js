3b37ba1ca86ee07b7309113ef8af705867288bf0
Ian Noyes <ian@12southmusic.com>
2014-10-15 14:25:48 -0500

    try ths

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,7 +299,7 @@ var Roots = {
     
   }
   
-  enquire.register("not handheld and (orientation : landscape) and (max-width : 992px), screen and (min-width: 992px)", function() {
+  enquire.register("not handheld and (orientation : landscape) and (max-width : 992px)", function() {
         $window.resize(function(){ //if the user resizes the window...
       Move(); //move the background images in relation to the movement of the scrollbar
     });   
