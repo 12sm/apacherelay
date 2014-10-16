1e5379c4c894466ed0301cb8983763c843abf391
Ian Noyes <ian@12southmusic.com>
2014-10-16 16:09:21 -0500

    getting specific

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,7 +184,7 @@ var Roots = {
     };
   }
 
-  enquire.register("only screen and (max-width : 992px), only screen and (max-width : 992px) and (orientation : landscape), (orientation : landscape)", {
+  enquire.register("only screen and (max-width : 992px), only screen and (max-width : 992px) and (orientation : landscape), (orientation : landscape) and (min-device-width : 768px) and (max-device-width : 1024px) ", {
     match: function() {
       noLax();
     },
